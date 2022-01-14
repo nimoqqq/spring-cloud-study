3e77e574450f58b6633bcdba6f86f3de21ca8edc
chufan <907701820@qq.com>
2022-01-14 16:07:23 +0800

    eureka

--- eureka/cloud-eureka-client/src/test/java/com/nimo/cloudeurekaclient/CloudEurekaClientApplicationTests.java
+++ eureka/cloud-eureka-client/src/test/java/com/nimo/cloudeurekaclient/CloudEurekaClientApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.eureka;
+package com.nimo.cloudeurekaclient;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EurekaApplicationTests {
+class CloudEurekaClientApplicationTests {
 
     @Test
     void contextLoads() {
---
+++ eureka/cloud-eureka-sever/src/test/java/com/nimo/cloudeurekasever/CloudEurekaSeverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nimo.cloudeurekasever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudEurekaSeverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
