8abdd334edfb4dbb6259e8ea73e52984a08361e6
chufan <907701820@qq.com>
2022-01-14 14:25:13 +0800

    frist commit

---
+++ src/test/java/com/nimo/springcloudstudy/SpringCloudStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nimo.springcloudstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
